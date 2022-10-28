36deb06edc223340aed1c3e14a03d480f03c9362
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-10-28 15:13:55 +0300

    fixed a bug regarding the json file
    previously i wouldn't specify the full path of the json file when opening it, now i do which allows me to add it to crontab.

--- main.py
+++ main.py
@@ -5,36 +5,36 @@ import change_values_of_num
 import create_logo
 
 def change_values_of_files():
-    with open('files.json') as json_file:
-        data = json.load(json_file)
-
     current_directory = os.path.dirname(os.path.realpath(__file__))
     all_files_in_folder = os.listdir(current_directory)
+
+    with open(current_directory+'/files.json') as json_file:
+        data = json.load(json_file)
+
     print(all_files_in_folder)
 
     counter = int(data['date_and_time'][1]['num_to_use_file'])
     if_updated = False
 
-    breakpoint()
     for f in all_files_in_folder:
-        print(f)
         if ".jpg" in f:
+            print(f)
             to_continue_loop = False
             new_counter = 0
-            
-            for i in data['files'][0]:
-                if f in data['files'][0]["file_"+str(new_counter)]:
-                    to_continue_loop = True
-                    break
-                new_counter = new_counter + 1
+            if len(data['files'][0]) != 0:
+                for i in data['files'][0]:
+                    if f in data['files'][0]["file_"+str(new_counter)]:
+                        to_continue_loop = True
+                        break
+                    new_counter = new_counter + 1
 
             if to_continue_loop == True:
                 continue
 
             if_updated = True
-            append_to_json = {"file_"+str(counter+1):f}
+            append_to_json = {"file_"+str(counter):f}
             counter = counter + 1
-            print(append_to_json)
+            print("append to json = ",append_to_json)
             data['files'][0].update(append_to_json)
 
     if if_updated == True:
