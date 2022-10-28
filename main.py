866bb5dd52ac32e3f622de8ba678867c57e3fadb
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-10-28 18:38:00 +0300

    removed all the prints

--- main.py
+++ main.py
@@ -11,14 +11,11 @@ def change_values_of_files():
     with open(current_directory+'/files.json') as json_file:
         data = json.load(json_file)
 
-    print(all_files_in_folder)
-
     counter = int(data['date_and_time'][1]['num_to_use_file'])
     if_updated = False
 
     for f in all_files_in_folder:
         if ".jpg" in f:
-            print(f)
             to_continue_loop = False
             new_counter = 0
             if len(data['files'][0]) != 0:
@@ -34,7 +31,6 @@ def change_values_of_files():
             if_updated = True
             append_to_json = {"file_"+str(counter):f}
             counter = counter + 1
-            print("append to json = ",append_to_json)
             data['files'][0].update(append_to_json)
 
     if if_updated == True:
@@ -42,7 +38,8 @@ def change_values_of_files():
             data['files'][0].update(append_to_json)
             json.dump(data, fp = fpn, indent=2)
     else:
-        print("no files")
+        pass
+        # print("no files")
 
 change_values_of_files()
 change_values_of_num.change_values_of_num()
