3d59d2d11fc4624d27d37a735844d13a38ca94d0
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-11-02 20:08:23 +0200

    added comments

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ def change_values_of_files():
         data = json.load(json_file)
 
     counter = int(data['date_and_time'][1]['num_to_use_file'])
+    # we only dump data to json if we added a file
     if_updated = False
-
+   
     for f in all_files_in_folder:
         if ".jpg" in f:
             to_continue_loop = False
+            # the written file were currently checking
             new_counter = 0
+
+            # this loop checks if the file is already written, if it is, we mark to_continue_loop as true and continue to the next file
             if len(data['files'][0]) != 0:
                 for i in data['files'][0]:
                     if f in data['files'][0]["file_"+str(new_counter)]:
@@ -37,9 +41,11 @@ def change_values_of_files():
         with open('files.json', 'w') as fpn:
             data['files'][0].update(append_to_json)
             json.dump(data, fp = fpn, indent=2)
-    else:
-        pass
+        fpn.close()
+    # else:
         # print("no files")
+
+    json_file.close()
 
 change_values_of_files()
 change_values_of_num.change_values_of_num()
