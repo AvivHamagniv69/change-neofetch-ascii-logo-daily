2c51a21c16f5abc55fc66e5619826e8573b999e2
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-11-03 23:01:56 +0200

    Update change_values_of_num.py

--- change_values_of_num.py
+++ change_values_of_num.py
@@ -14,14 +14,18 @@ def change_values_of_num(amount_of_files):
     current_time = datetime.datetime.now()
     current_day = current_time.day
 
-    with open('files.json', 'w') as fpn:
+    if current_day != data['date_and_time'][0]['date']:
+        with open('files.json', 'w') as fpn:
+            data['date_and_time'][0]['date'] = current_day
+
+            # if the num of file we want to use is bigger then the amount of images we can use then we circle back to 0
+            if amount_of_files <= num+1:
+                data['date_and_time'][1]['num_to_use_file'] = 0
+
+            else:
+                data['date_and_time'][1]['num_to_use_file'] = num+1
+            
+            json.dump(data, fp = fpn, indent=2)
+
+    else:
         data['date_and_time'][0]['date'] = current_day
-
-        # if the num of file we want to use is bigger then the amount of images we can use then we circle back to 0
-        if amount_of_files <= num+1:
-            data['date_and_time'][1]['num_to_use_file'] = 0
-
-        else:
-            data['date_and_time'][1]['num_to_use_file'] = num+1
-        
-        json.dump(data, fp = fpn, indent=2)
