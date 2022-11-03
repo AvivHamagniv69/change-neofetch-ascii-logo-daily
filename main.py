3c0170d2e3b9628cb7334e873e736816660ca3df
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-11-03 23:03:36 +0200

    solved a bug
    solved a bug where the image will change only after running the script 2 times because of the logic of the image we told the program to draw

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import json
 import change_values_of_num
 import create_logo
 
+def count_the_amt_files():
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    all_files_in_folder = os.listdir(current_directory)
+    amount_of_files = 0
+
+    for i in all_files_in_folder:
+        if ".jpg" in i:
+            amount_of_files = amount_of_files + 1
+
+    return amount_of_files
+
 def change_values_of_files():
     current_directory = os.path.dirname(os.path.realpath(__file__))
     all_files_in_folder = os.listdir(current_directory)
@@ -15,21 +26,18 @@ def change_values_of_files():
     # we only dump data to json if we added a file
     if_updated = False
     counter_for_jpg = 0
-    amount_of_files = 0
-
-    for i in all_files_in_folder:
-        if ".jpg" in i:
-            amount_of_files = amount_of_files + 1
 
     for f in all_files_in_folder:
         if ".jpg" in f:
-            if counter == counter_for_jpg:
-                return f, amount_of_files
+            if counter_for_jpg == counter:
+                return f
 
             counter_for_jpg = counter_for_jpg + 1
 
     json_file.close()
 
-f, amount_of_files = change_values_of_files()
+amount_of_files = count_the_amt_files()
 change_values_of_num.change_values_of_num(amount_of_files)
+
+f = change_values_of_files()
 create_logo.create_logo(f)
