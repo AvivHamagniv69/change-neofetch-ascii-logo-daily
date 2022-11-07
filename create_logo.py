e8590ae3fb1e6854a4b11a8cc47e061cd082c2b0
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-11-07 19:16:03 +0200

    no try except anymore, take absolute value of size, no space in ascii table, we dont need tkinter

--- create_logo.py
+++ create_logo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from PIL import ImageTk, Image
+from PIL import Image
 from math import floor
 import os
 import json
@@ -23,7 +23,7 @@ def create_logo(file_to_use):
     width_before_resize, height_before_resize = im.size
     ratio = width_before_resize/height_before_resize
     ratio = round(ratio)
-    size = int(sys.argv[1])
+    size = abs(int(sys.argv[1]))
     num_to_decide_ratio = 4
 
     # if the image ratio/2 is smaller then 1 then dividing it will just make it way bigger
@@ -38,29 +38,30 @@ def create_logo(file_to_use):
     new_im = im.resize((round(size*num_to_decide_ratio), round(size)))
 
     file_of_ascii = open(current_directory+"/new_logo.txt", "w")
-    ascii_chars = ".,:;+*?%#@S "
+    ascii_chars = ".,:;+*?%#@S"
     ascii_chars = list(ascii_chars)
     len_ascii = len(ascii_chars)
     width, height = new_im.size
     num_to_multiply = 1
 
     # we pass over the image before we draw the ascii image because otherwise it has a chance of going out of range
-    try:
-        for n in range(height):
-            for i in range(width):
-                pixelRGB = new_im.getpixel((i,n))
-                if "(" not in str(pixelRGB):
-                    ascii_to_write = ascii_chars[floor(pixelRGB/(len_ascii*num_to_multiply))]
-                    continue
-                
+    for n in range(height):
+        for i in range(width):
+            pixelRGB = new_im.getpixel((i,n))
+            if "(" not in str(pixelRGB):
+                if floor(pixelRGB/(len_ascii*num_to_multiply)) >= len_ascii:
+                    num_to_multiply = num_to_multiply+1
+                    n = 0
+                    i = 0
+
+            else:
                 r,g,b = pixelRGB
                 brightness = (r+g+b)/3 
-                ascii_to_write = ascii_chars[floor(brightness/(len_ascii*num_to_multiply))]
 
-    except:
-        num_to_multiply = num_to_multiply+0.5
-        n = 0
-        i = 0
+                if floor(brightness/(len_ascii*num_to_multiply)) >= len_ascii:
+                    num_to_multiply = num_to_multiply+1
+                    n = 0
+                    i = 0
 
     for h in range(height):
         for row in range(width):
@@ -69,18 +70,15 @@ def create_logo(file_to_use):
             if "(" not in str(pixelRGB):
                 ascii_to_write = ascii_chars[floor(pixelRGB/(len_ascii*num_to_multiply))]    
                 file_of_ascii.write(ascii_to_write)
-                continue
 
-            r,g,b = pixelRGB
-            brightness = (r+g+b)/3 
-
-            if (floor(brightness/(len_ascii*num_to_multiply))) >= len_ascii:
-                file_of_ascii.write(ascii_chars[len_ascii-1])
-                continue
-                
-            ascii_to_write = ascii_chars[floor(brightness/(len_ascii*num_to_multiply))]
-            file_of_ascii.write(ascii_to_write)
+            else:
+                r,g,b = pixelRGB
+                brightness = (r+g+b)/3 
+                ascii_to_write = ascii_chars[floor(brightness/(len_ascii*num_to_multiply))]
+                file_of_ascii.write(ascii_to_write)
 
         file_of_ascii.write("\n")
 
     file_of_ascii.close()
+    json_file.close()
+    im.close()
